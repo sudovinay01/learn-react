4b39d8982239ae1552b151d889ce7cc29c824c43
Vinay Kumar Maheshwaram <54681400+sudovinay01@users.noreply.github.com>
2025-02-16 14:32:02 +0000

    feat: update Vite configuration and enhance App component with Dev Containers support

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,8 +15,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://containers.dev/" target="_blank">
+          <img src="https://ms-vscode-remote.gallerycdn.vsassets.io/extensions/ms-vscode-remote/remote-containers/0.398.0/1738859433725/Microsoft.VisualStudio.Services.Icons.Default" className="logo dev container" alt="Dev container logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Dev Containers</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,9 +27,12 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>
+          This project is enabled with dev containers.
+        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logos to learn more
       </p>
     </>
   )
